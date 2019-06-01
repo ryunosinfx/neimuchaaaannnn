1104c067e5019ee8ed119630ee0488e4a32cdbdc
ryunosinfx <ryunosinfxxul@gmail.com>
2019-06-01 22:43:03 +0900

    フルスクリーンWebkit対応

--- src/js/util/FullScreen.ts
+++ src/js/util/FullScreen.ts
@@ -1,8 +1,17 @@
+interface webkitHTMLElement extends HTMLElement {
+  webkitRequestFullScreen(): void;
+  requestFullScreen(): void;
+}
+
 export class FullScreen {
   // 全画面表示の実行
   static enter() {
-    const body = document.body;
-    body.requestFullscreen();
+    const body: webkitHTMLElement = <webkitHTMLElement>document.body;
+    if (body.webkitRequestFullScreen) {
+      body.webkitRequestFullScreen();
+    } else {
+      body.requestFullScreen();
+    }
   }
   static exit() {
     if (document.exitFullscreen) {
